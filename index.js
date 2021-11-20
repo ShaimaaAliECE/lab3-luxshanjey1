aac4cb8265ba108672967d5215391ae6402539db
ljeyaran1 <ljeyaran@uwo.ca>
2021-11-20 14:55:42 -0500

    created signin page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const newConnection = require('./DBConnection')
 const app = express(); //returns instance of a server
 
 //serve staic contents
-app.use(expre.static('static'));
+app.use(express.static('static'));
 //dynaminc handling
 
 
@@ -18,9 +18,9 @@ app.get('/login', (request, response) => {
     conn.query(`select * from SignIn`
                 ,
                 (err, rows, fields) => {
-                    if(err)
+                    if(err){
                         console.log(err)
-                    else 
+                    }else{ 
                         let signIn = rows;
                         //if password and username match the one in the database
                         if(request.query.username == signIn[0].username && request.query.password == signIn[0].password)
@@ -28,6 +28,7 @@ app.get('/login', (request, response) => {
                         else 
                             response.redirect('/signIn') //if wrong refresh page
                 }
+            }
                 );
     conn.end();
 });
@@ -90,7 +91,7 @@ app.get('/adminPage', (request, response) => {
                 ${d.Time9 ? "green" : "grey"}'></td>`;
                 content += `<td style = 'background-color: 
                 ${d.Time10 ? "green" : "grey"}'></td>`;
-                content += "</tr>";
+                content += '</tr>';
             }
                 content += "</table>";
                 //a link to the time change page
@@ -221,4 +222,122 @@ app.get('/changeTime', (request, response) => {
     conn.end();
     }
 );
+
+//page for guests
+app.get('/guestPage', (request, response) => {
+    let conn = newConnection();
+    conn.connect();
+
+    conn.query('select * from Time', 
+        (err, rows, fields) => {
+            let content = '';
+
+            let headers = rows;
+            content += '<tr>';
+            content += `<th>${headers[0].t0}</th>`;
+            content += `<th>${headers[0].t1}</th>`;
+            content += `<th>${headers[0].t2}</th>`;
+            content += `<th>${headers[0].t3}</th>`;
+            content += `<th>${headers[0].t4}</th>`;
+            content += `<th>${headers[0].t5}</th>`;
+            content += `<th>${headers[0].t6}</th>`;
+            content += `<th>${headers[0].t7}</th>`;
+            content += `<th>${headers[0].t8}</th>`;
+            content += `<th>${headers[0].t9}</th>`;
+            content += '</tr>';
+
+            response.write(content);
+
+            if(err)
+                console.log(err);
+        });
+    
+    conn.query('select * from Doodle',
+        (err,row, fields) => {
+            let data = rows;
+            let content = '';
+            for(d of data){
+                content += '<tr>';
+                content += `<td>${d.Name}</td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time1 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time2 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time3 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time4 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time5 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time6 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time7 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time8 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time9 ? "green" : "grey"}'></td>`;
+                content += `<td style = 'background-color: 
+                ${d.Time10 ? "green" : "grey"}'></td>`;
+                content += '</tr>';
+            }
+
+            //update table within the form
+            content += '<tr>';
+            content += `<form action = '/add-data' id = 'form'>
+                            <td><input name = "name"/></td>
+                            <td><input type = 'checkbox' name = "t0" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t1" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t2" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t3" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t4" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t5" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t6" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t7" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t8" form = 'form'/></td>
+                            <td><input type = 'checkbox' name = "t9" form = 'form'/></td>
+                        </form>`;
+            content += '</tr>';
+            content += '</table>';
+            content += "<input type = 'submit' value = 'save' form = 'form'/> ";
+            
+            response.write(content);
+
+            response.end();
+        }
+    );
+    conn.end();
+    
+});
+
+app.get('/addData', (request, response) => {
+    let conn = newConnection();
+    conn.connect();
+
+    conn.query(
+        //if the value exists then it is true, else it is false
+        `insert into Doodle values (
+            '${req.query.name}',
+            '${req.query.t0 ? 1 : 0},
+            '${req.query.t1 ? 1 : 0}',
+            '${req.query.t2 ? 1 : 0}',
+            '${req.query.t3 ? 1 : 0}',
+            '${req.query.t4 ? 1 : 0}',
+            '${req.query.t5 ? 1 : 0}',
+            '${req.query.t6 ? 1 : 0}',
+            '${req.query.t7 ? 1 : 0}',
+            '${req.query.t8 ? 1 : 0}',
+            '${req.query.t9 ? 1 : 0}'
+            )`,
+        (err, rows, fields) => {
+          res.redirect("/guestPage");
+          if (err) 
+            console.log(err);
+        }
+      );
+    
+      conn.end();
+})
+
+
 app.listen(80);
